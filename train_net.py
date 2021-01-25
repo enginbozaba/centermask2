a3c5fc490f4bb454ee59786d3ba5f538c09e26ad
Engin Bozaba <enginbozaba@gmail.com>
2021-01-25 17:51:19 +0300

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -147,6 +147,23 @@ def main(args):
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
+    
+    #https://github.com/youngwanLEE/centermask2/issues/36
+
+    # Regist own dataset.
+    from detectron2.data.datasets import register_coco_instances
+    # train data
+    name        = "path_train"
+    json_file   = "/content/centermask2/datasets/coco/annotations/instances_train2017.json"
+    image_root  = "/content/centermask2/datasets/coco/train2017"
+    # test data
+    name_val        = "path_val"
+    json_file_val   = "/content/centermask2/datasets/coco/annotations/instances_val2017.json"
+    image_root_val  = "/content/centermask2/datasets/coco/val2017"
+    # registr -- ilk çağrıldığında kullan
+    #register_coco_instances(name, {}, json_file, image_root)
+    #register_coco_instances(name_val, {}, json_file_val, image_root_val)
+    
     launch(
         main,
         args.num_gpus,
