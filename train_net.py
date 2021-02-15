80129dddb2d442b2b7f18d3feec5088583ec8a11
Engin Bozaba <enginbozaba@gmail.com>
2021-02-15 14:43:28 +0300

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -154,12 +154,12 @@ if __name__ == "__main__":
     from detectron2.data.datasets import register_coco_instances
     # train data
     name        = "path_train"
-    json_file   = "./centermask2/datasets/coco/annotations/train_anno.json"
-    image_root  = "./centermask2/datasets/coco/images/train"
+    json_file   = "/content/centermask2/datasets/nuclei/annotations/train_anno.json"
+    image_root  = "/content/centermask2/datasets/nuclei/images"
     # test data
     name_val        = "path_val"
-    json_file_val   = "./centermask2/datasets/coco/annotations/val_anno.json"
-    image_root_val  = "./centermask2/datasets/coco/images/val"
+    json_file_val   = "/content/centermask2/datasets/nuclei/annotations/train_anno.json"
+    image_root_val  = "/content/centermask2/datasets/nuclei/images"
     #registr -- ilk çağrıldığında kullan
     register_coco_instances(name, {}, json_file, image_root)
     register_coco_instances(name_val, {}, json_file_val, image_root_val)
